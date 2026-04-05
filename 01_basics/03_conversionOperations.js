0feb1d6323bf06ec1064c1626856ab2c2fcc3c56
Sumit Bhasagi <bhasagisumit@gmail.com>
2026-04-05 15:39:05 +0530

    Conversion Operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,36 @@
+// let score = "33abc";
+// let score = null;
+
+let score = undefined;
+
+// console.log(score);
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+//console.log(valueInNumber); // yaha pe kya ayega output ?? --> NaN(not a number)
+
+//console.log(typeof valueInNumber);
+
+//console.log(typeof score);
+
+// "33" --> 33
+//  "33abc" --> Nan (not a number)
+
+let isloggedIn = 1; // (1,2,3,....) --> true  ("") --> false ("anything") --> true (0)--> flase
+
+let booleanIsLoggedIn = Boolean(isloggedIn); //datatype conversion: number to boolean
+
+// console.log(booleanIsLoggedIn);
+// console.log(isloggedIn);
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber); // datatype conversion: number to string
+console.log(someNumber);
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+console.log(typeof someNumber);

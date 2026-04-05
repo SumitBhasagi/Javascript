6795ab0cd724462e0e566a488fa055e362c9bf87
Sumit Bhasagi <bhasagisumit@gmail.com>
2026-04-05 17:22:56 +0530

    Comparison Operators

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -29,8 +29,58 @@ let booleanIsLoggedIn = Boolean(isloggedIn); //datatype conversion: number to bo
 let someNumber = 33;
 
 let stringNumber = String(someNumber); // datatype conversion: number to string
-console.log(someNumber);
-console.log(stringNumber);
+// console.log(someNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
-console.log(typeof someNumber);
+// console.log(typeof stringNumber);
+// console.log(typeof someNumber);
+
+////////////// ******** OPERATIONS ******** ////////////////////////
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ^ 3);  // 2 XOR 3 = 1
+// console.log(2 ** 3); // 2 raised to 3 = 8
+// console.log(2 % 3);  // remainder
+
+let str1 = "Hello";
+let str2 = "Sumit";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + 2); //output --> 3
+// console.log("1" + 2); // output --> 12
+// console.log(1 + "2"); // output --> 12
+// console.log("1" + "2"); // output --> 12
+
+// console.log("1" + 2 + 2); //output --> 122
+// console.log(2 + 2 + "1"); //output --> 41
+
+// console.log(3 + ((4 * 5) % 3));
+// console.log(((3 + 4) * 5) % 3);
+
+// console.log(true); // output --> true
+// console.log(+true); // output --> 1
+// console.log(+""); // output --> 0
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter; // prefix
+
+console.log(gameCounter);
+
+gameCounter++;  // postfix 
+
+console.log(gameCounter);
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log(2 > "1");
+// console.log("02" > 1);
+
+// console.log(null > 0); // output -->  false
+// console.log(null == 0); // output -->  false
+// console.log(null >= 0); // output -->  true
+
+// console.log(undefined == 0); //output --> false // kaise bhi aaur kisise bhi compare kro output false hi aayega
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+
+// strict check === ///
+
+// console.log("2" === 2); // output --> false
+// console.log("2" === "2"); // output --> true

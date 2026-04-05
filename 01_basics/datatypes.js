1d2894fa7093afc0bd9dba507f26997caad56eac
Sumit Bhasagi <bhasagisumit@gmail.com>
2026-04-05 20:31:51 +0530

    Stack and Heap Memory in JS

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -32,6 +32,45 @@ const myFunction = function () {
   console.log("Hello World!!");
 };
 
-myFunction();
+// myFunction(); // calling the function
 
-console.log(typeof heros);
+// console.log(typeof heros);
+
+// ************ Memory in JS *********** //
+
+// Stack (Primitive), Heap (Non-Primitive)
+//
+// primitive datatypes humesha stack memory mein store hote hain
+// Non primitive datatypes humesha heap memory mein store hote hain
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let myYoutubeName = "ZerosandOnes";
+
+let anotherName = myYoutubeName;
+anotherName = "KuchBhi";
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+// stack memory mein agar aap kuch access krne ke liye jaatein hain toh wo original value ka copy deta hain
+//
+// similarly yahape upar anotherName ko pehle ek copy mili thi
+// jab usne wo change kiya toh uss copy ki jagah uski value aagayi
+
+let userOne = {
+  email: "user@google.com",
+  upiId: "user@axl",
+};
+
+let userTwo = userOne;
+
+userTwo.email = "newUser@google.com";
+
+console.log(userOne.email); //output --> newUser@google.com
+console.log(userTwo.email); //output --> newUser@google.com
+
+// since we are using non-primitive datatypes we are storing these arrays and objects in heap
+// and here in heap if we access something then wo apne ko uss particular chhez ka reference deta hain
+
+// usiliye yahape userOne.email and userTwo.email ka output same aaraha hain, bcoz wo ek hi object ke do alag naam hai

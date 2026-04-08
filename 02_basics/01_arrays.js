34b9974e45c7899eb4b09d8ddc7588a124fbbf2e
Sumit Bhasagi <bhasagisumit@gmail.com>
2026-04-08 12:29:23 +0530

    arrays started

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+// aarays
+
+// const myarr = [0, 1, 2, 3, 4, 5, true, sumit];  // he pn valid array aahe
+
+const myArr = [0, 1, 2, 3, 4, 5];
+
+console.log(myArr[0]);
+
+
+// shallow copies --> share the same reference points
+
+//deep copies --> do not share the same reference 
+

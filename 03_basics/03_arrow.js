1e8d11a599b15485b3e1ca4c81bec7932f22ce26
Sumit Bhasagi <bhasagisumit@gmail.com>
2026-04-10 19:46:17 +0530

    Arrow Function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,41 @@
+const user = {
+  username: "Pratik",
+  price: 999,
+
+  welcomeMessage: function () {
+    console.log(`${this.username}, Welcome to website`);
+    console.log(this);
+  },
+};
+
+// user.welcomeMessage();
+
+// user.username = "Akhilesh";
+
+// user.welcomeMessage();
+
+// Arrow Function:  it is similar to the normal function but with different syntax
+
+const addTwo = (num1, num2) => {
+  return num1 + num2;
+};
+
+// shortform to write the same above function -->
+
+const add = (a, b) => a + b;
+
+// console.log(addTwo(3, 5));
+
+// addTwo(5, 5);
+
+const myfunc = (num1, num2) => {
+  {
+    username: "sumit";
+  }
+};
+
+console.log(myfunc(4, 5));
+
+const myarray = [1, 2, 3, 4, 5];
+
+
